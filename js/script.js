f1f1f2b9481dad5ae84cbced8ca9864d3f827964
AlexanderSemenov92 <42918575+AlexanderSemenov92@users.noreply.github.com>
2019-10-29 16:32:32 +0200

    end sections

---
+++ js/script.js
@@ -0,0 +1,29 @@
+// document.addEventListener('contextmenu', event => event.preventDefault());
+
+// document.onkeypress = function (event) {
+//     event = (event || window.event);
+//     if (event.keyCode == 123) {
+//        //alert('No F-12');
+//         return false;
+//     }
+// }
+// document.onmousedown = function (event) {
+//     event = (event || window.event);
+//     if (event.keyCode == 123) {
+//         //alert('No F-keys');
+//         return false;
+//     }
+// }
+// document.onkeydown = function (event) {
+//     event = (event || window.event);
+//     if (event.keyCode == 123) {
+//         //alert('No F-keys');
+//         return false;
+//     }
+// }
+
+// document.addEventListener('keydown', function (event) {
+//     if (event.ctrlKey && event.keyCode == 85 || event.ctrlKey && event.shiftKey && event.keyCode == 73 || event.keyCode == 123) {
+//         event.preventDefault();
+//     }
+// })
